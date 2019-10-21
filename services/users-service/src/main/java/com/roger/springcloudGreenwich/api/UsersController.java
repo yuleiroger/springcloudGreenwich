e11ee561b7bef0cfaae254d17098b1660292bd74
yulei <yulei@beyondcent.com>
2019-10-21 16:30:29 +0800

    update

--- services/users-service/src/main/java/com/roger/springcloudGreenwich/api/UsersController.java
+++ services/users-service/src/main/java/com/roger/springcloudGreenwich/api/UsersController.java
@@ -1,5 +1,6 @@
 package com.roger.springcloudGreenwich.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by admin on 2019/10/21.
  */
 @RestController
+@Slf4j
 @RequestMapping("/users-service")
 public class UsersController {
     @GetMapping(value = "/login")
     public String login(){
+        log.info("login");
         return "login success";
     }
 }
