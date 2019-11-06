66c4d12246185b4adc6c4e0653db73c73de357f1
yulei <yulei@beyondcent.com>
2019-11-06 14:44:38 +0800

    first

---
+++ services/users-service/src/main/java/com/roger/springcloudGreenwich/component/UtilComponent.java
@@ -0,0 +1,22 @@
+package com.roger.springcloudGreenwich.component;
+
+import com.roger.springcloudGreenwich.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by admin on 2019/11/6.
+ */
+@Component
+public class UtilComponent {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    @Bean
+    public RedisUtil userServiceRedisUtil(){
+        return new RedisUtil(redisTemplate);
+    }
+}
