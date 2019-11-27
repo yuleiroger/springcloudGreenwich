01569c120de708963bd93c7c4979d5ea870c8818
yulei <yulei@beyondcent.com>
2019-11-27 14:56:12 +0800

    first commit

---
+++ common-entities/src/main/java/com/roger/springcloudGreenwich/User.java
@@ -0,0 +1,17 @@
+package com.roger.springcloudGreenwich;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Created by admin on 2019/10/25.
+ */
+@Data
+public class User implements Serializable{
+    private static final long serialVersionUID = -2859466854096196299L;
+    private Long id;
+    private String userNo;
+    private String userName;
+    private String password;
+}
