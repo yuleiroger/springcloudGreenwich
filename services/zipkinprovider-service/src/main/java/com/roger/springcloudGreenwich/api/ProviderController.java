4577b5b2689e51e868003ca83e38d9b6b3fa6c85
yulei <yulei@beyondcent.com>
2019-12-13 15:58:45 +0800

    zipkin

--- services/zipkinprovider-service/src/main/java/com/roger/springcloudGreenwich/api/ProviderController.java
+++ services/zipkinprovider-service/src/main/java/com/roger/springcloudGreenwich/api/ProviderController.java
@@ -1,5 +1,6 @@
 package com.roger.springcloudGreenwich.api;
 
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
